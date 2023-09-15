234a21699353dcead27d775b32f223009b47901b
akilsalah <akilsalaheddine203@gmail.com>
2023-09-16 00:23:50 +0200

    condition challenges

---
+++ EX1_condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   printf("entrer le numbre : ");
+   scanf("%d",&a);
+    
+    if (a%2==0){
+        printf("le nombre %d est paire",a);
+
+    }else{
+
+         printf("le nombre %d est impaire",a);
+    }
+    return 0;
+}
