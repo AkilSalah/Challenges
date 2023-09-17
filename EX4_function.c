60fcf2d2a1fdfab119f7df4c0e74c71aeffc9398
akilsalah <akilsalaheddine203@gmail.com>
2023-09-17 02:08:48 +0200

    max function

--- EX4_function.c
+++ EX4_function.c
@@ -1,57 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void max_4(int a, int b, int c,int d){
-    int max;
-    printf("entrer 4 nombre : ");
-    scanf("%d",a);
-    scanf("%d",b);
-    scanf("%d",c);
-    scanf("%d",d);
-    if (a>b && a>c && a>d){
-        max=a;
-    }else if (b>a && b>c && b>d){
-        max=b;
-    }else if (c>a && c>b && c>d){
-        max=c;
-    }else
-     {  max=d;
-    }
-  
-    printf("le max est %d : ",max);
+int max_4(int a, int b, int c,int d){
+    int m;
+    int max(int,int);
+    m=max(max(a,b),max(c,d));
+    return m;
 }
-void max(int a, int b){
-    int max;
-    printf("entrer 2 nombre : ");
-    scanf("%d%d",a,b);
+int max(int a, int b){
     if (a>b ){
-        max=a;
-    }else {
-        max=b;
-    }
-    printf("le max est %d :",max);
+        return a;
+     } else 
+         return b;
+    
 }
 
 int main()
 {
   int x,y,z,q,k;
-    printf("1.pour le max de 4 entier ");
-    printf("2.pour le max de 4 entier ");
-    scanf("%d",k);
-    switch (k)
-    {
-    case 1:
-         max_4(x,y,z,q);
-        break;
-    case 2:
-        max(x,y); 
-        break;
-    
-    default:
-    printf("INTROUVABLE");
-        break;
-    }
-   
-    
+    printf("enter 4 nombre \n");
+    scanf("%d%d%d%d",&x,&y,&z,&q);
+    k=max_4(x,y,z,q);
+    printf("le max est %d",k);
     return 0;
 }
